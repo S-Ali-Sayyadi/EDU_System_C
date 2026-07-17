d67a15e24631e1cf246311a042c49d237c920f57
Ali Sayyadi <sayyadiseyyedali@gmail.com>
2026-07-17 14:44:09 +0330

    feat: add core data structures

--- edu_system.c
+++ edu_system.c
@@ -1,5 +1,101 @@
 #include <stdio.h>
 
+#define MAX_STUDENTS 1000
+#define MAX_FACULTY 500
+#define MAX_COURSES 500
+#define MAX_OFFERINGS 1000
+#define MAX_REQUESTS 1000
+#define MAX_ENROLLED 400
+#define STR_SIZE 128
+#define SMALL_SIZE 64
+
+typedef struct
+{
+    char student_id[SMALL_SIZE];
+    double grade;
+    int survey_score;
+} Enrollment;
+
+typedef struct
+{
+    char first_name[STR_SIZE];
+    char last_name[STR_SIZE];
+    char student_id[SMALL_SIZE];
+    char national_code[SMALL_SIZE];
+    char field[STR_SIZE];
+    int entrance_year;
+    char section[SMALL_SIZE];
+    char mentor[STR_SIZE];
+    char department[STR_SIZE];
+    char answer_birth[STR_SIZE];
+    char answer_book[STR_SIZE];
+    char answer_bike[STR_SIZE];
+    char password[STR_SIZE];
+} Student;
+
+typedef struct
+{
+    char first_name[STR_SIZE];
+    char last_name[STR_SIZE];
+    char faculty_id[SMALL_SIZE];
+    char national_code[SMALL_SIZE];
+    char field[STR_SIZE];
+    int entrance_year;
+    char degree[STR_SIZE];
+    char department[STR_SIZE];
+    char password[STR_SIZE];
+    char answer_birth[STR_SIZE];
+    char answer_book[STR_SIZE];
+    char answer_bike[STR_SIZE];
+} Faculty;
+
+typedef struct
+{
+    char name[STR_SIZE];
+    char course_id[SMALL_SIZE];
+    int units;
+    char prerequisites[STR_SIZE];
+    char section[SMALL_SIZE];
+    char field[STR_SIZE];
+    char department[STR_SIZE];
+} Course;
+
+typedef struct
+{
+    char course_id[SMALL_SIZE];
+    char faculty_id[SMALL_SIZE];
+    int semester;
+    int capacity;
+    int enrolled_count;
+    char department[STR_SIZE];
+    char place[STR_SIZE];
+    Enrollment enrollments[MAX_ENROLLED];
+} Offering;
+
+typedef struct
+{
+    int id;
+    char type[SMALL_SIZE];
+    char course_id[SMALL_SIZE];
+    char faculty_id[SMALL_SIZE];
+    int semester;
+    int capacity;
+    int amount;
+    int offering_index;
+    char department[STR_SIZE];
+    char place[STR_SIZE];
+    char status[SMALL_SIZE];
+} Request;
+
+typedef struct
+{
+    int offering;
+    int unit_selection;
+    int classes_exams;
+    int grade_recording;
+    int course_survey;
+} CalendarState;
+
 static int read_int(const char *prompt);
 static void show_main_menu(void);
 
